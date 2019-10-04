994aba245107a36bba2ddd342454df19cf252fd7
Joao Pedro <joaopedrobeckland@gmail.com>
2019-10-04 20:16:15 -0300

    Layouts and Login Page

---
+++ src/pages/_layouts/auth/styles.js
@@ -0,0 +1,67 @@
+import styled from 'styled-components';
+import { darken } from 'polished';
+
+export const Wrapper = styled.div`
+  height: 100%;
+  background: linear-gradient(-180deg, #18448F, #F8E738);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  max-width: 315px;
+  text-align: center;
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 30px;
+    img {
+      width: 48px;
+      height: 48px;
+    }
+
+    input {
+      background: rgba(0, 0, 0, 0.1);
+      border: 0;
+      border-radius: 4px;
+      height: 44px;
+      padding: 0 15px;
+      color: #fff;
+      margin: 0 0 10px;
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.7);
+      }
+    }
+    span {
+      color: #fff;
+      align-self: flex-start;
+      margin: 0 0 10px;
+      font-weight: bold;
+    }
+    button {
+      margin: 5px 0 0;
+      height: 44px;
+      background: #18448F;
+      font-weight: bold;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      font-size: 16px;
+      transition: background 0.2s;
+      &:hover {
+        background: ${darken(0.03, '#18448F')};
+      }
+    }
+    a {
+      color: #fff;
+      margin-top: 15px;
+      font-size: 16px;
+      opacity: 0.8;
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+`;
---
+++ src/pages/_layouts/default/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 150%;
+  background: linear-gradient(-90deg, #18448F, #F8E738);
+`;
